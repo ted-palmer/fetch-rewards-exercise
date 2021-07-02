727e093ea1749bb7452636db3535a6247cb5e75c
ted-palmer <etpalmer17@gmail.com>
2021-07-02 16:52:53 -0400

    Filtered items by listId

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import { render } from "@testing-library/react";
+
 import React, { useState, useEffect } from "react";
+import Table from 'react-bootstrap/Table'
 const App = () => {
 
-  let [list, setList] = useState(null) 
+  const [list, setList] = useState([]);
 
-    //Fetch data
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await fetch("https://fetch-hiring.s3.amazonaws.com/hiring.json");
-          let data = await response.json();
-          let filteredData = data.filter(item => item.name)
-          setList(filteredData);
-          console.log(data)
-        }
+  //Fetch data
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+        const data = await response.json();
 
-        catch (e) {
-          console.log(e)
-        }
-        
+        //Filter out any items where "name" is blank or null
+        const filteredData = data.filter((item) => item.name);
+        setList(filteredData);
+
+      } catch (e) {
+        console.log(e);
       }
-      fetchData();
-  },[])
+    }
+    fetchData();
+  }, []);
 
-
-
+  //Filter items by listId
+  const list1 = list.filter((item) => item.listId === 1);
+  const list2 = list.filter((item) => item.listId === 2);
+  const list3 = list.filter((item) => item.listId === 3);
+  const list4 = list.filter((item) => item.listId === 4);
 
   return (
     <div>
-     {list && list.map((item) => 
-     <div>
-     <p>Name: {item.name}</p>
-     <p>ID: {item.id}</p>
-     <p>ListId: {item.listId}</p>
-     </div>
-      )}
-    </div> 
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>ListId</th>
+            <th>Name</th>
+          </tr>
+          <tbody>
+            {list1 &&
+              list1.map((item) => (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.listId}</td>
+                  <td>{item.name}</td>
+                </tr>
+              ))}
+          </tbody>
+        </thead>
+      </Table>
+    </div>
   );
-}
+};
 
 export default App;
