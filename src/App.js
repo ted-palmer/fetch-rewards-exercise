e7b3659665df2da8a81d93baad98cd282efa5181
ted-palmer <etpalmer17@gmail.com>
2021-07-02 16:14:05 -0400

    Setup fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, { useState, useEffect } from "react";
+const App = () => {
 
-function App() {
+  let [list, setList] = useState(null) 
+
+    //Fetch data
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await fetch("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+          const data = await response.json();
+          setList(data);
+          console.log(data)
+        }
+
+        catch (e) {
+          console.log(e)
+        }
+        
+      }
+      fetchData();
+  },[])
+
+
+
   return (
     <div>
-     
+     {list && list.map((item) => <p>{item.name}</p>)}
+     {/* {list && JSON.stringify(list)} */}
     </div>
   );
 }
