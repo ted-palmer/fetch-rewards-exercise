696552c2fd30e379c2c26c7d8ab942cfe0741580
ted-palmer <etpalmer17@gmail.com>
2021-07-03 09:58:11 -0400

    Added ant design and setup tables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from "react";
-import Table from 'react-bootstrap/Table'
+import { Table } from 'antd';
 const App = () => {
 
   const [list, setList] = useState([]);
@@ -9,13 +9,18 @@ const App = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+        const response = await fetch(
+          "https://fetch-hiring.s3.amazonaws.com/hiring.json"
+        );
         const data = await response.json();
 
-        //Filter out any items where "name" is blank or null
-        const filteredData = data.filter((item) => item.name);
+        //Filter out items where "name" is null and sort by listId or name
+        const filteredData = data
+          .filter((item) => item.name)
+          .sort(function (a, b) {
+            return a.listId - b.listId || +a.name.slice(5) - +b.name.slice(5);
+          });
         setList(filteredData);
-
       } catch (e) {
         console.log(e);
       }
@@ -29,27 +34,35 @@ const App = () => {
   const list3 = list.filter((item) => item.listId === 3);
   const list4 = list.filter((item) => item.listId === 4);
 
+  //Columns for table
+  const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'listId',
+      dataIndex: 'listId',
+      key: 'listId',
+    },
+    {
+      title: 'name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+  ];
+
   return (
-    <div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>ListId</th>
-            <th>Name</th>
-          </tr>
-          <tbody>
-            {list1 &&
-              list1.map((item) => (
-                <tr>
-                  <td>{item.id}</td>
-                  <td>{item.listId}</td>
-                  <td>{item.name}</td>
-                </tr>
-              ))}
-          </tbody>
-        </thead>
-      </Table>
+    <div className="container">
+      <h1>List 1:</h1>
+     <Table dataSource={list1} columns={columns} size="small" rowKey="Id1"/>
+     <h1>List 2:</h1>
+     <Table dataSource={list2} columns={columns} size="small" rowKey="Id2"/>
+     <h1>List 3:</h1>
+     <Table dataSource={list3} columns={columns} size="small" rowKey="Id3"/>
+     <h1>List 4:</h1>
+     <Table dataSource={list4} columns={columns} size="small" rowKey="Id4"/>
     </div>
   );
 };
